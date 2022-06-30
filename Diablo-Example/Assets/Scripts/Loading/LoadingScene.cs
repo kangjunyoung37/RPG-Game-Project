65d0b0009bb4da97ebc635b4002c3a9575dbd585
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-06-30 23:15:15 +0900

    #로딩시 Firebase에서 데이터가 자동으로 넘어오게 설정

--- Diablo-Example/Assets/Scripts/Loading/LoadingScene.cs
+++ Diablo-Example/Assets/Scripts/Loading/LoadingScene.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Firebase.Database;
+using kang.InventorySystem.Inventory;
 public class LoadingScene : MonoBehaviour
 {
     const float TextUpdateInterval = 1.0f;
     const string LoadingTextValue = "...";
     const float NextSceneInterval = 3.0f;
-    
+
+    private DatabaseReference databaseRef;
+    private string UserDataPath => "users";// /users/
+    private string StatsDataPath => "stats";// /users/uid/stats
+    private string EquipmentDataPath => "equipment"; // /users/uid/equipment
+    private string InventoryDataPath => "inventory"; // /users/uid/inventory
+
+    public StatsObject playerStats;
+    public InventoryObject playerEquipment;
+    public InventoryObject playerInventory;
 
     [SerializeField]
     private TMP_Text LoadingText;
@@ -18,7 +29,10 @@ public class LoadingScene : MonoBehaviour
     float LoadingTime;
     private void Start()
     {
+        databaseRef = FirebaseDatabase.DefaultInstance.RootReference;
         LoadingTime = Time.time;
+        OnclickedLoad();
+
     }
 
 
@@ -44,5 +58,67 @@ public class LoadingScene : MonoBehaviour
         {
             SceneController.Instance.LoadScene(SceneNameConstants.MainScene);
         }
+    }
+    public void OnclickedLoad()
+    {
+        var userId = FireBaseAuthController.Instance.UserId;
+        if (userId == string.Empty)
+        {
+            return;
+        }
+
+        databaseRef.Child(UserDataPath).Child(userId).Child(StatsDataPath).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("Load user data was canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Load user data encouneterd an error : " + task.Exception);
+                return;
+            }
+            DataSnapshot snapshot = task.Result;
+            playerStats.FromJson(snapshot.GetRawJsonValue());
+            Debug.LogFormat("Load User data in successfully : {0} {1}", userId, snapshot.GetRawJsonValue());
+        });
+
+        databaseRef.Child(UserDataPath).Child(userId).Child(EquipmentDataPath).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("Load Equipment data was canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Load Equipment data encouneterd an error : " + task.Exception);
+                return;
+            }
+            DataSnapshot snapshot = task.Result;
+            playerEquipment.FromJson(snapshot.GetRawJsonValue());
+            Debug.LogFormat("Load Equipment data in successfully : {0} {1}", userId, snapshot.GetRawJsonValue());
+        });
+
+        string inventoryJson = playerInventory.ToJson();
+        databaseRef.Child(UserDataPath).Child(userId).Child(InventoryDataPath).GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("Load Inventory data was canceled");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Load Inventory data encouneterd an error : " + task.Exception);
+                return;
+            }
+
+            DataSnapshot snapshot = task.Result;
+            playerInventory.FromJson(snapshot.GetRawJsonValue());
+            Debug.LogFormat("Load Inventory data in successfully : {0} {1}", userId, snapshot.GetRawJsonValue());
+        });
+        Debug.Log("OK");
     }
 }
